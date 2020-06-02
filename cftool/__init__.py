26917125d9cfb1a3ccfdf2a9b708da528279d42c
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-06-03 01:43:22 +0800

    Update __init__.py

--- cftool/__init__.py
+++ cftool/__init__.py
@@ -5,9 +5,9 @@ from .manage import *
 
 __all__ = [
     "get_indices_from_another", "get_unique_indices", "get_one_hot", "hash_code", "prefix_dict",
-    "timestamp", "fix_float_to_length", "truncate_string_to_length", "grouped", "is_numeric", "show_or_save",
-    "update_dict", "Grid", "Saving", "Anneal", "Metrics", "ScalarEMA", "LoggingMixin", "PureLoggingMixin",
-    "SavingMixin", "context_error_handler", "timeit", "lock_manager", "batch_manager", "timing_context",
-    "prod", "shallow_copy_dict", "register_core", "PCManager", "GPUManager",
-    "ResourceManager", "data_tuple_saving_controller"
+    "timestamp", "fix_float_to_length", "truncate_string_to_length", "grouped", "is_numeric", "update_dict",
+    "show_or_save", "show_or_return", "Grid", "Saving", "Anneal", "Metrics", "ScalarEMA", "Grid", "Visualizer",
+    "LoggingMixin", "PureLoggingMixin", "SavingMixin", "context_error_handler", "timeit", "lock_manager",
+    "batch_manager", "timing_context", "prod", "shallow_copy_dict", "register_core",
+    "data_tuple_saving_controller", "PCManager", "GPUManager", "ResourceManager"
 ]

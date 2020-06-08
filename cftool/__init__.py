7e7a045eb6da5a7b6f41dd64f1d5f6baefaf9a94
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-06-08 22:34:46 +0800

    Update __init__.py
    去掉了 misc 以外的模块，以避免暴露过多功能

--- cftool/__init__.py
+++ cftool/__init__.py
@@ -1,14 +1,10 @@
-from .ml import *
 from .misc import *
-from .manage import *
 
 
 __all__ = [
     "get_indices_from_another", "get_unique_indices", "get_one_hot", "hash_code", "prefix_dict",
     "timestamp", "fix_float_to_length", "truncate_string_to_length", "grouped", "is_numeric", "update_dict",
-    "show_or_save", "show_or_return", "Grid", "Saving", "Anneal", "Metrics", "Estimator", "ScalarEMA", "Visualizer",
-    "LoggingMixin", "PureLoggingMixin", "SavingMixin", "context_error_handler", "timeit", "lock_manager",
-    "batch_manager", "timing_context", "prod", "shallow_copy_dict", "register_core",
-    "data_tuple_saving_controller", "PCManager", "GPUManager", "ResourceManager",
-    "get_counter_from_arr", "allclose", "Sampler"
+    "show_or_save", "show_or_return", "Saving", "LoggingMixin", "PureLoggingMixin", "SavingMixin", "Sampler",
+    "context_error_handler", "timeit", "lock_manager", "batch_manager", "timing_context", "prod",
+    "get_counter_from_arr", "allclose", "shallow_copy_dict", "register_core", "data_tuple_saving_controller"
 ]

d63c34e65a78873e0abd8f92a900cd22600765d6
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-06-09 11:12:19 +0800

    Update __init__.py
    将 param_utils 的模块暴露在了 cftool.ml 中

--- cftool/ml/__init__.py
+++ cftool/ml/__init__.py
@@ -1,9 +1,13 @@
 from .hpo import *
 from .utils import *
+from .param_utils import *
 
 
 __all__ = [
     "HPOBase", "hpo_dict",
     "Anneal", "Metrics", "ScalarEMA", "Visualizer",
-    "Estimator", "ModelPattern", "EnsemblePattern", "Comparer"
+    "Estimator", "ModelPattern", "EnsemblePattern", "Comparer",
+    "ParamsGenerator",
+    "DataType", "Iterable", "Any", "Int", "Float", "Bool", "String",
+    "DistributionBase", "Uniform", "Exponential", "Choice"
 ]

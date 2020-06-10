b8475ac3cedd6368434e29178468f14623e91569
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-06-10 23:01:53 +0800

    将 BayesianOptimization 暴露在了 cftool.optim 中

--- cftool/optim/__init__.py
+++ cftool/optim/__init__.py
@@ -1,4 +1,8 @@
+from .bo import *
 from .gd import *
 
 
-__all__ = ["Optimizer", "GradientDescentMixin", "optimizer_dict"]
+__all__ = [
+    "BayesianOptimization",
+    "Optimizer", "GradientDescentMixin", "optimizer_dict"
+]

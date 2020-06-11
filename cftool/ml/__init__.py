9d12c2c3fb6ac958309da3b3960ae9121f4029f7
heyujian <syameimaru_kurumi@pku.edu.cn>
2020-06-11 17:05:26 +0800

    实现了 register_metric

--- cftool/ml/__init__.py
+++ cftool/ml/__init__.py
@@ -3,11 +3,19 @@ from .utils import *
 from .param_utils import *
 
 
+def register_metric(name, sign, requires_prob):
+    def _register(f):
+        Metrics.add_metric(f, name, sign, requires_prob)
+        return f
+    return _register
+
+
 __all__ = [
     "HPOBase", "hpo_dict",
     "Anneal", "Metrics", "ScalarEMA", "Visualizer",
     "Estimator", "ModelPattern", "EnsemblePattern", "Comparer",
     "ParamsGenerator",
     "DataType", "Iterable", "Any", "Int", "Float", "Bool", "String",
-    "DistributionBase", "Uniform", "Exponential", "Choice"
+    "DistributionBase", "Uniform", "Exponential", "Choice",
+    "register_metric"
 ]

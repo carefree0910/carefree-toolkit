9ecdc86a17c921f3698e2cc5b2af0f1328b24779
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2020-09-14 23:54:21 +0800

    去除了冗余代码

--- cftool/ml/__init__.py
+++ cftool/ml/__init__.py
@@ -1,13 +1,6 @@
 from .utils import *
 
 
-def register_metric(name, sign, requires_prob):
-    def _register(f):
-        Metrics.add_metric(f, name, sign, requires_prob)
-        return f
-    return _register
-
-
 __all__ = [
     "Anneal", "Metrics", "ScalarEMA", "Visualizer", "Tracker",
     "Estimator", "ModelPattern", "EnsemblePattern", "Comparer",

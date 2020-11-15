aca410de986d033dd5dd16be6c7dd70891d70e3d
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2020-11-15 19:28:26 +0800

    修复了 `naive_rolling_*`

--- cftool/c/cython_substitute.py
+++ cftool/c/cython_substitute.py
@@ -4,11 +4,15 @@ from ..misc import StrideArray
 
 
 def naive_rolling_min(array: np.ndarray, window: int, axis: int = -1) -> np.ndarray:
-    return np.nanmin(StrideArray(array).roll(window, axis=axis), axis)
+    while axis < 0:
+        axis += len(array.shape)
+    return np.nanmin(StrideArray(array).roll(window, axis=axis), axis + 1)
 
 
 def naive_rolling_max(array: np.ndarray, window: int, axis: int = -1) -> np.ndarray:
-    return np.nanmax(StrideArray(array).roll(window, axis=axis), axis)
+    while axis < 0:
+        axis += len(array.shape)
+    return np.nanmax(StrideArray(array).roll(window, axis=axis), axis + 1)
 
 
 __all__ = ["naive_rolling_min", "naive_rolling_max"]

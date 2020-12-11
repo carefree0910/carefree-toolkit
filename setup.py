988d352a035ab7317e9f7bfb577c4f35d72565bb
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2020-12-11 15:33:19 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import platform
 from Cython.Build import cythonize
 from setuptools import setup, find_packages, Extension
 
-VERSION = "0.2.3"
+VERSION = "0.2.4"
 
 DESCRIPTION = "Some commonly used functions and modules"
 with open("README.md") as f:

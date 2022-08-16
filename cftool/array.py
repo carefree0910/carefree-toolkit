d096da8864bcaf340001b215c4badaf486bb746e
carefree0910 <syameimaru.saki@gmail.com>
2022-08-16 11:23:26 +0800

    实现了 `is_string`

--- cftool/array.py
+++ cftool/array.py
@@ -36,6 +36,10 @@ def is_float(arr: np.ndarray) -> bool:
     return np.issubdtype(arr.dtype, np.floating)
 
 
+def is_string(arr: np.ndarray) -> bool:
+    return np.issubdtype(arr.dtype, str)
+
+
 def sigmoid(arr: arr_type) -> arr_type:
     if isinstance(arr, np.ndarray):
         return 1.0 / (1.0 + np.exp(-arr))

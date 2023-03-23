e0b61b8c307e22e47b8560171c874a1ecc3208a7
carefree0910 <syameimaru.saki@gmail.com>
2023-03-23 17:20:22 +0800

    兼容了 PIL 未安装的情况

--- cftool/cv.py
+++ cftool/cv.py
@@ -1,23 +1,33 @@
 import numpy as np
 
 from io import BytesIO
-from PIL import Image
 from numpy import ndarray
 from typing import Tuple
 
 from .array import torch
 from .array import arr_type
 
+try:
+    from PIL import Image
 
-def to_rgb(
-    image: Image.Image,
-    color: Tuple[int, int, int] = (255, 255, 255),
-) -> Image.Image:
+    TImage = Image.Image
+except:
+    Image = None
+    TImage = None
+try:
+    import cv2
+except:
+    cv2 = None
+
+
+def to_rgb(image: TImage, color: Tuple[int, int, int] = (255, 255, 255)) -> TImage:
     if image.mode == "CMYK":
         return image.convert("RGB")
     split = image.split()
     if len(split) < 4:
         return image.convert("RGB")
+    if Image is None:
+        raise ValueError("`pillow` is needed for `to_rgb`")
     background = Image.new("RGB", image.size, color)
     background.paste(image, mask=split[3])
     return background
@@ -30,6 +40,8 @@ def to_uint8(normalized_img: arr_type) -> arr_type:
 
 
 def np_to_bytes(img_arr: ndarray) -> bytes:
+    if Image is None:
+        raise ValueError("`pillow` is needed for `np_to_bytes`")
     if img_arr.dtype != np.uint8:
         img_arr = to_uint8(img_arr)
     bytes_io = BytesIO()

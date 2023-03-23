a35639fd61c70d8012fe424e355fed217b05056a
carefree0910 <syameimaru.saki@gmail.com>
2023-03-23 17:28:32 +0800

    实现了 `read_image`

--- cftool/cv.py
+++ cftool/cv.py
@@ -2,9 +2,17 @@ import numpy as np
 
 from io import BytesIO
 from numpy import ndarray
+from typing import Any
+from typing import Dict
+from typing import Type
 from typing import Tuple
+from typing import Union
 from typing import Optional
+from typing import NamedTuple
 
+from .misc import safe_execute
+from .misc import shallow_copy_dict
+from .misc import WithRegister
 from .array import torch
 from .array import arr_type
 
@@ -19,6 +27,16 @@ try:
     import cv2
 except:
     cv2 = None
+
+
+padding_modes: Dict[str, Type["Padding"]] = {}
+
+
+class ReadImageResponse(NamedTuple):
+    image: np.ndarray
+    alpha: Optional[np.ndarray]
+    original: TImage
+    original_size: Tuple[int, int]
 
 
 def to_rgb(image: TImage, color: Tuple[int, int, int] = (255, 255, 255)) -> TImage:
@@ -48,6 +66,82 @@ def np_to_bytes(img_arr: ndarray) -> bytes:
     bytes_io = BytesIO()
     Image.fromarray(img_arr).save(bytes_io, format="PNG")
     return bytes_io.getvalue()
+
+
+def restrict_wh(w: int, h: int, max_wh: int) -> Tuple[int, int]:
+    max_original_wh = max(w, h)
+    if max_original_wh <= max_wh:
+        return w, h
+    wh_ratio = w / h
+    if wh_ratio >= 1:
+        return max_wh, round(max_wh / wh_ratio)
+    return round(max_wh * wh_ratio), max_wh
+
+
+def get_suitable_size(n: int, anchor: int) -> int:
+    if n <= anchor:
+        return anchor
+    mod = n % anchor
+    return n - mod + int(mod > 0.5 * anchor) * anchor
+
+
+def read_image(
+    image: Union[str, TImage],
+    max_wh: Optional[int],
+    *,
+    anchor: Optional[int],
+    to_gray: bool = False,
+    to_mask: bool = False,
+    resample: Any = Image.LANCZOS,
+    normalize: bool = True,
+    padding_mode: Optional[str] = None,
+    padding_kwargs: Optional[Dict[str, Any]] = None,
+    to_torch_fmt: bool = True,
+) -> ReadImageResponse:
+    if Image is None:
+        raise ValueError("`pillow` is needed for `read_image`")
+    if isinstance(image, str):
+        image = Image.open(image)
+    alpha = None
+    original = image
+    if image.mode == "RGBA":
+        alpha = image.split()[3]
+    if not to_mask and not to_gray:
+        if alpha is None or padding_mode is None:
+            image = to_rgb(image)
+        else:
+            padding = Padding.make(padding_mode, {})
+            padding_kw = shallow_copy_dict(padding_kwargs or {})
+            padding_kw.update(dict(image=image, alpha=alpha))
+            image = safe_execute(padding.pad, padding_kw)
+    else:
+        if to_mask and to_gray:
+            raise ValueError("`to_mask` & `to_gray` should not be True simultaneously")
+        if to_mask and image.mode == "RGBA":
+            image = alpha
+        else:
+            image = image.convert("L")
+    original_w, original_h = image.size
+    if max_wh is None:
+        w, h = original_w, original_h
+    else:
+        w, h = restrict_wh(original_w, original_h, max_wh)
+    if anchor is not None:
+        w, h = map(get_suitable_size, (w, h), (anchor, anchor))
+    image = image.resize((w, h), resample=resample)
+    image = np.array(image)
+    if normalize:
+        image = image.astype(np.float32) / 255.0
+    if alpha is not None:
+        alpha = np.array(alpha)[None, None]
+        if normalize:
+            alpha = alpha.astype(np.float32) / 255.0
+    if to_torch_fmt:
+        if to_mask or to_gray:
+            image = image[None, None]
+        else:
+            image = image[None].transpose(0, 3, 1, 2)
+    return ReadImageResponse(image, alpha, original, (original_w, original_h))
 
 
 class ImageProcessor:
@@ -107,3 +201,47 @@ class ImageProcessor:
                 transformed[rev_mask] = reference_channel[rev_mask]
             transformed_channels.append(transformed)
         return np.stack(transformed_channels, axis=2)
+
+
+class Padding(WithRegister):
+    d = padding_modes
+
+    @abstractmethod
+    def pad(self, image: Image.Image, alpha: Image.Image, **kwargs: Any) -> Image.Image:
+        pass
+
+
+@Padding.register("cv2_ns")
+class CV2NS(Padding):
+    def pad(
+        self,
+        image: Image.Image,
+        alpha: Image.Image,
+        *,
+        radius: int = 5,
+        **kwargs: Any,
+    ) -> Image.Image:
+        if cv2 is None:
+            raise ValueError("`cv2` is needed for `CV2NS`")
+        img_arr = np.array(image.convert("RGB"))[..., ::-1]
+        mask_arr = np.array(alpha)
+        rs = cv2.inpaint(img_arr, 255 - mask_arr, radius, cv2.INPAINT_NS)
+        return Image.fromarray(rs[..., ::-1])
+
+
+@Padding.register("cv2_telea")
+class CV2Telea(Padding):
+    def pad(
+        self,
+        image: Image.Image,
+        alpha: Image.Image,
+        *,
+        radius: int = 5,
+        **kwargs: Any,
+    ) -> Image.Image:
+        if cv2 is None:
+            raise ValueError("`cv2` is needed for `CV2Telea`")
+        img_arr = np.array(image.convert("RGB"))[..., ::-1]
+        mask_arr = np.array(alpha)
+        rs = cv2.inpaint(img_arr, 255 - mask_arr, radius, cv2.INPAINT_TELEA)
+        return Image.fromarray(rs[..., ::-1])

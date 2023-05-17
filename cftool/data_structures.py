d973932a5c9e468e5a4566a33b1055804daa6b41
carefree0910 <syameimaru.saki@gmail.com>
2023-05-17 21:12:25 +0800

    实现了 `ILoadablePool`

--- cftool/data_structures.py
+++ cftool/data_structures.py
@@ -1,3 +1,6 @@
+import gc
+import time
+
 from typing import Any
 from typing import Dict
 from typing import List
@@ -5,12 +8,18 @@ from typing import Type
 from typing import Tuple
 from typing import Generic
 from typing import TypeVar
-from typing import Optional
+from typing import Callable
 from typing import Iterator
+from typing import Optional
+
+from .misc import print_info
+from .misc import sort_dict_by_value
+from .constants import TIME_FORMAT
 
 
 TItemData = TypeVar("TItemData")
 TTypes = TypeVar("TTypes")
+TItem = TypeVar("TItem")
 
 
 class Item(Generic[TItemData]):
@@ -200,3 +209,69 @@ class Types(Generic[TTypes]):
 
     def values(self) -> Iterator[Type[TTypes]]:
         return self._types.values()  # type: ignore
+
+
+class ILoadableItem(Generic[TItem]):
+    _item: Optional[TItem]
+
+    def __init__(self, init_fn: Callable[[], TItem], *, init: bool = False):
+        self.init_fn = init_fn
+        self.load_time = time.time()
+        self._item = init_fn() if init else None
+
+    def load(self, **kwargs: Any) -> TItem:
+        self.load_time = time.time()
+        if self._item is None:
+            self._item = self.init_fn()
+        return self._item
+
+    def unload(self) -> None:
+        self._item = None
+        gc.collect()
+
+
+class ILoadablePool(Generic[TItem]):
+    pool: Dict[str, ILoadableItem]
+    activated: Dict[str, ILoadableItem]
+
+    # set `limit` to negative values to indicate 'no limit'
+    def __init__(self, limit: int = -1):
+        self.pool = {}
+        self.activated = {}
+        self.limit = limit
+        if limit == 0:
+            raise ValueError(
+                "limit should either be negative "
+                "(which indicates 'no limit') or be positive"
+            )
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.pool
+
+    def register(self, key: str, init_fn: Callable[[bool], ILoadableItem]) -> None:
+        if key in self.pool:
+            raise ValueError(f"key '{key}' already exists")
+        init = self.limit < 0 or len(self.activated) < self.limit
+        loadable_item = init_fn(init)
+        self.pool[key] = loadable_item
+        if init:
+            self.activated[key] = loadable_item
+
+    def get(self, key: str, **kwargs: Any) -> TItem:
+        loadable_item = self.pool.get(key)
+        if loadable_item is None:
+            raise ValueError(f"key '{key}' does not exist")
+        item = loadable_item.load(**kwargs)
+        if key in self.activated:
+            return item
+        load_times = {k: v.load_time for k, v in self.activated.items()}
+        earliest_key = list(sort_dict_by_value(load_times).keys())[0]
+        self.activated.pop(earliest_key).unload()
+        self.activated[key] = loadable_item
+
+        time_format = "-".join(TIME_FORMAT.split("-")[:-1])
+        print_info(
+            f"'{earliest_key}' is unloaded to make room for '{key}' "
+            f"(last updated: {time.strftime(time_format, time.localtime(loadable_item.load_time))})"
+        )
+        return item

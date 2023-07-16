56515f1336158e56200502545a09c012dbd516cd
carefree0910 <syameimaru.saki@gmail.com>
2023-07-16 23:56:53 +0800

    增强了 `noli`

--- cftool/noli.py
+++ cftool/noli.py
@@ -36,8 +36,16 @@ class LayerParams(BaseModel):
     z: float
 
 
+class RenderType(str, Enum):
+    FIT = "fit"
+    FILL = "fill"
+    COVER = "cover"
+
+
 class RenderParams(BaseModel):
     src: Optional[str]
+    renderType: Optional[RenderType]
+    cropFields: Optional[Matrix2D]
 
 
 class SingleNode(BaseModel):
@@ -159,11 +167,51 @@ def parse_graph(render_info_list: List[Dict[str, Any]]) -> Graph:
     return Graph(root_nodes=list(map(parse_node, render_info_list)))
 
 
+# specific utility functions
+
+DEFAULT_RENDER_TYPE = RenderType.COVER
+
+
+def get_img_transform(w: float, h: float, render_params: RenderParams) -> Matrix2D:
+    img_transform = Matrix2D.scale_matrix(w, h)
+    crop_transform = render_params.cropFields
+    if crop_transform is None:
+        return img_transform
+    return img_transform @ crop_transform
+
+
+def get_img_render_transform(
+    bbox: Matrix2D,
+    img_w: float,
+    img_h: float,
+    render_params: RenderParams,
+) -> Matrix2D:
+    img_transform = get_img_transform(img_w, img_h, render_params)
+    render_transform = img_transform.inverse
+    render_type = render_params.renderType or DEFAULT_RENDER_TYPE
+    if render_type == RenderType.FILL:
+        return render_transform
+    w, h = bbox.wh
+    h = max(1.0e-8, abs(h))
+    area_wh_ratio = w / h
+    img_wh_ratio = img_w / img_h
+    ratio = img_wh_ratio / area_wh_ratio
+    is_fit = render_type == RenderType.FIT
+    if (ratio >= 1.0 and not is_fit) or (ratio < 1.0 and is_fit):
+        w_scale, h_scale = ratio, 1.0
+    else:
+        w_scale, h_scale = 1.0, 1.0 / ratio
+    scale_center = Matrix2D.identical().center
+    render_transform = render_transform.scale(w_scale, h_scale, scale_center)
+    return render_transform
+
+
 __all__ = [
     "SingleNodeType",
     "GroupType",
     "INodeType",
     "LayerParams",
+    "RenderType",
     "RenderParams",
     "SingleNode",
     "Group",
@@ -171,4 +219,6 @@ __all__ = [
     "Graph",
     "parse_node",
     "parse_graph",
+    "get_img_transform",
+    "get_img_render_transform",
 ]

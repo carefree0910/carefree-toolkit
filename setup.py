c277824b34a5f126db7d0c1ff03ce73b5a54f57f
carefree0910 <syameimaru.saki@gmail.com>
2023-12-28 16:55:57 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-VERSION = "0.3.10"
+VERSION = "0.3.11"
 
 DESCRIPTION = "Some commonly used functions and modules"
 with open("README.md") as f:
